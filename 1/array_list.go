2fc32c8bf0130b47b83c233968c9c11ef8138412
Mikhail Dyakov <wtltl2@gmail.com>
2015-12-22 12:37:51 +0100

    ArrayList works with any type.

--- 1/array_list.go
+++ 1/array_list.go
@@ -1,29 +1,32 @@
 package base
 
+// Element represents a single element in array
+type Element interface{}
+
 // ArrayList implements something close to go slice
 type ArrayList struct {
-	arr     []int
+	arr     []Element
 	size    int
 	arrSize int
 }
 
 // NewArrayList create an ArrayList of specific size
 func NewArrayList(size int) ArrayList {
-	return ArrayList{make([]int, size), size, size}
+	return ArrayList{make([]Element, size), size, size}
 }
 
 // Get returns a value from specific index
-func (a *ArrayList) Get(index int) int {
+func (a *ArrayList) Get(index int) Element {
 	a.checkRange(index)
 
 	return a.arr[index]
 }
 
 // Set sets the value under specific index
-func (a *ArrayList) Set(index int, val int) {
+func (a *ArrayList) Set(index int, val interface{}) {
 	a.checkRange(index)
 
-	a.arr[index] = val
+	a.arr[index] = Element(val)
 }
 
 // Len returns a length of the array
@@ -32,16 +35,16 @@ func (a *ArrayList) Len() int {
 }
 
 // Append appends a new value to the end of the array
-func (a *ArrayList) Append(val int) {
+func (a *ArrayList) Append(val interface{}) {
 	if a.size == a.arrSize {
 		a.arrSize = a.arrSize*2 + 1
 		old := a.arr
-		a.arr = make([]int, a.arrSize)
+		a.arr = make([]Element, a.arrSize)
 		copy(a.arr, old)
 	}
 
 	a.size++
-	a.arr[a.size-1] = val
+	a.arr[a.size-1] = Element(val)
 }
 
 func (a *ArrayList) checkRange(index int) {
--- 1/array_list_test.go
+++ 1/array_list_test.go
@@ -7,8 +7,10 @@ import (
 func TestGetSet(t *testing.T) {
 	a := NewArrayList(3)
 	a.Set(0, 20)
-	if a.Get(0) != 20 {
-		t.Errorf("Expected 20 at index 0")
+	var out int
+	out = a.Get(0).(int)
+	if out != 20 {
+		t.Errorf("Expected 20 at index 0, got %d", out)
 	}
 }
 
@@ -17,10 +19,10 @@ func TestAppend(t *testing.T) {
 	a.Append(10)
 	a.Append(20)
 	a.Append(30)
-	if a.Get(0) != 10 {
+	if a.Get(0).(int) != 10 {
 		t.Errorf("Expected 10 at index 0")
 	}
-	if a.Get(2) != 30 {
+	if a.Get(2).(int) != 30 {
 		t.Errorf("Expected 30 at index 2")
 	}
 }
